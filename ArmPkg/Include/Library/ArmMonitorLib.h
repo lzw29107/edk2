98ff8b805780e9224cfabbbf596fff526b29dd3c
lzw29107 <2910729822@qq.com>
2026-01-02 17:05:54 +0800

    Revert "ArmPkg: Remove ARM32 Support from ArmMonitorLib"
    This reverts commit 889676ac603a209376193ee87a1e5c15fe6e53a2.

--- ArmPkg/Include/Library/ArmMonitorLib.h
+++ ArmPkg/Include/Library/ArmMonitorLib.h
@@ -23,6 +23,7 @@ typedef struct {
   UINTN    Arg5;
   UINTN    Arg6;
   UINTN    Arg7;
+ #ifdef MDE_CPU_AARCH64
   UINTN    Arg8;
   UINTN    Arg9;
   UINTN    Arg10;
@@ -33,6 +34,7 @@ typedef struct {
   UINTN    Arg15;
   UINTN    Arg16;
   UINTN    Arg17;
+ #endif
 } ARM_MONITOR_ARGS;
 
 /** Monitor call.

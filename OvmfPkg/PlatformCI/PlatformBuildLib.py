a83bbe9834bfcf5797a8ddb07c3670c98e5548af
lzw_29107 <2910729822@qq.com>
2026-08-21 16:02:56 +0800

    Revert "OvmfPkg: Remove OVMF IA32"
    This reverts commit 1fb88ffe284782cc79e306306b8d19829b6248b7.

--- OvmfPkg/PlatformCI/PlatformBuildLib.py
+++ OvmfPkg/PlatformCI/PlatformBuildLib.py
@@ -123,10 +123,10 @@ class PlatformBuilder( UefiBuilder, BuildSettingsManager):
 
     def AddCommandLineOptions(self, parserObj):
         ''' Add command line options to the argparser '''
-        parserObj.add_argument('-a', "--arch", dest="build_arch", type=str, default="X64",
-            help="Optional - CSV of architecture to build. "
+        parserObj.add_argument('-a', "--arch", dest="build_arch", type=str, default="IA32,X64",
+            help="Optional - CSV of architecture to build.  IA32 will use IA32 for Pei & Dxe. "
             "X64 will use X64 for both PEI and DXE.  IA32,X64 will use IA32 for PEI and "
-            "X64 for DXE. Default is X64")
+            "X64 for DXE. default is IA32,X64")
 
     def RetrieveCommandLineOptions(self, args):
         '''  Retrieve command line options from the argparser '''

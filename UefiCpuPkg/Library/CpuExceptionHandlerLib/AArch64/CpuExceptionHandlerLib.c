afe6d9f6ad91d811886fff26bb6debd038cc2ab0
lzw_29107 <2910729822@qq.com>
2026-08-21 14:24:30 +0800

    Revert "ArmPkg: Remove ARM32 Support from ArmExceptionLib"
    This reverts commit 79e9dee6c7849a5f9ee473ae18ebef4f9ade187b.

--- UefiCpuPkg/Library/CpuExceptionHandlerLib/AArch64/CpuExceptionHandlerLib.c
+++ UefiCpuPkg/Library/CpuExceptionHandlerLib/AArch64/CpuExceptionHandlerLib.c
@@ -20,7 +20,6 @@
 #include <Protocol/DebugSupport.h>
 
 #include <CpuExceptionHelpers.h>
-#include "ExceptionSupport.h"
 
 //
 // Maximum number of characters to print to serial (UINT8s) and to console if
@@ -210,6 +209,11 @@ BaseName (
   return Str;
 }
 
+VOID
+RegisterEl0Stack (
+  IN  VOID  *Stack
+  );
+
 /**
   Register EL0 stack and perform architecture specific configuration to ensure
   exceptions are routed to correct EL.
@@ -244,45 +248,13 @@ ArchVectorConfig (
 }
 
 /**
-  Common C-level exception handler dispatched from ASM exception entry points.
-  Invokes a registered handler for the given ExceptionType if one exists,
-  otherwise dumps the CPU context. Asserts on unknown exception types.
-
-  @param[in]      ExceptionType  AArch64 exception type or interrupt vector number.
-  @param[in,out]  SystemContext  Saved CPU context at the time of the exception.
-**/
-VOID
-EFIAPI
-CommonCExceptionHandler (
-  IN     EFI_EXCEPTION_TYPE  ExceptionType,
-  IN OUT EFI_SYSTEM_CONTEXT  SystemContext
-  )
-{
-  if ((UINTN)ExceptionType <= mMaxExceptionNumber) {
-    if (mExceptionHandlers[ExceptionType] != NULL) {
-      mExceptionHandlers[ExceptionType](ExceptionType, SystemContext);
-      return;
-    }
-  } else {
-    DEBUG ((DEBUG_ERROR, "Unknown exception type %d\n", ExceptionType));
-    ASSERT (FALSE);
-  }
-
-  DumpCpuContext (ExceptionType, SystemContext);
-}
-
-//
-// CpuExceptionHandler Implementation
-//
-
-/**
-  Dumps the full AArch64 CPU context to serial and ConOut on an unexpected
+  Dumps the full ARM/AArch64 CPU context to serial and ConOut on an unexpected
   exception, then asserts and halts. Detects recursive calls and short-circuits
   to a minimal serial write followed by CpuDeadLoop ().
 
   Do not allocate memory or perform complex operations from within this function.
 
-  @param[in]      ExceptionType  AArch64 exception type or interrupt vector number.
+  @param[in]      ExceptionType  ARM/AArch64 exception type or interrupt vector number.
   @param[in,out]  SystemContext  Saved CPU context at the time of the exception.
 **/
 VOID
@@ -437,118 +409,4 @@ DumpCpuContext (
 
   ASSERT (FALSE);
   CpuDeadLoop ();
-}
-
-/**
-Initializes all CPU exceptions entries and provides the default exception handlers.
-
-Caller should try to get an array of interrupt and/or exception vectors that are in use and need to
-persist by EFI_VECTOR_HANDOFF_INFO defined in PI 1.3 specification.
-If caller cannot get reserved vector list or it does not exists, set VectorInfo to NULL.
-If VectorInfo is not NULL, the exception vectors will be initialized per vector attribute accordingly.
-
-@param[in]  VectorInfo    Pointer to reserved vector list.
-
-@retval EFI_SUCCESS           CPU Exception Entries have been successfully initialized
-                              with default exception handlers.
-@retval EFI_INVALID_PARAMETER VectorInfo includes the invalid content if VectorInfo is not NULL.
-@retval EFI_UNSUPPORTED       This function is not supported.
-
-**/
-EFI_STATUS
-EFIAPI
-InitializeCpuExceptionHandlers (
-  IN EFI_VECTOR_HANDOFF_INFO  *VectorInfo OPTIONAL
-  )
-{
-  UINT64  VectorBase;
-
-  // use VBAR to point to where our exception handlers are
-
-  // The vector table must be aligned for the architecture.  If this
-  // assertion fails ensure the appropriate FFS alignment is in effect,
-  // which can be accomplished by ensuring the proper Align=X statement
-  // in the platform packaging rules.  For AArch64 Align=4K is required.
-  // Align=Auto can be used but this is known to cause an issue with
-  // populating the reset vector area for encapsulated FVs.
-  ASSERT (((UINTN)ExceptionHandlersStart & mExceptionVectorAlignmentMask) == 0);
-
-  VectorBase = (UINT64)(UINTN)ExceptionHandlersStart;
-
-  // call the architecture-specific routine to prepare for the new vector
-  // configuration to take effect
-  ArchVectorConfig ((UINTN)VectorBase);
-
-  ArmWriteVBar ((UINTN)VectorBase);
-
-  return EFI_SUCCESS;
-}
-
-/**
-Registers a function to be called from the processor exception handler. (On AArch64 this only
-provides exception handlers, not interrupt handling which is provided through the Hardware Interrupt
-Protocol.)
-
-This function registers and enables the handler specified by ExceptionHandler for a processor
-interrupt or exception type specified by ExceptionType. If ExceptionHandler is NULL, then the
-handler for the processor interrupt or exception type specified by ExceptionType is uninstalled.
-The installed handler is called once for each processor interrupt or exception.
-NOTE: This function should be invoked after InitializeCpuExceptionHandlers() is invoked,
-otherwise EFI_UNSUPPORTED returned.
-
-@param[in]  ExceptionType     Defines which interrupt or exception to hook.
-@param[in]  ExceptionHandler  A pointer to a function of type EFI_CPU_INTERRUPT_HANDLER that is called
-when a processor interrupt occurs. If this parameter is NULL, then the handler
-will be uninstalled.
-
-@retval EFI_SUCCESS           The handler for the processor interrupt was successfully installed or uninstalled.
-@retval EFI_ALREADY_STARTED   ExceptionHandler is not NULL, and a handler for ExceptionType was
-previously installed.
-@retval EFI_INVALID_PARAMETER ExceptionHandler is NULL, and a handler for ExceptionType was not
-previously installed.
-@retval EFI_UNSUPPORTED       The interrupt specified by ExceptionType is not supported,
-or this function is not supported.
-**/
-EFI_STATUS
-EFIAPI
-RegisterCpuInterruptHandler (
-  IN EFI_EXCEPTION_TYPE         ExceptionType,
-  IN EFI_CPU_INTERRUPT_HANDLER  ExceptionHandler
-  )
-{
-  if ((UINTN)ExceptionType > mMaxExceptionNumber) {
-    return EFI_UNSUPPORTED;
-  }
-
-  if ((ExceptionHandler != NULL) && (mExceptionHandlers[ExceptionType] != NULL)) {
-    return EFI_ALREADY_STARTED;
-  }
-
-  mExceptionHandlers[ExceptionType] = ExceptionHandler;
-
-  return EFI_SUCCESS;
-}
-
-/**
-  Setup separate stacks for certain exception handlers.
-  If the input Buffer and BufferSize are both NULL, use global variable if possible.
-
-  @param[in]       Buffer        Point to buffer used to separate exception stack.
-  @param[in, out]  BufferSize    On input, it indicates the byte size of Buffer.
-                                 If the size is not enough, the return status will
-                                 be EFI_BUFFER_TOO_SMALL, and output BufferSize
-                                 will be the size it needs.
-
-  @retval EFI_SUCCESS             The stacks are assigned successfully.
-  @retval EFI_UNSUPPORTED         This function is not supported.
-  @retval EFI_BUFFER_TOO_SMALL    This BufferSize is too small.
-**/
-EFI_STATUS
-EFIAPI
-InitializeSeparateExceptionStacks (
-  IN     VOID   *Buffer,
-  IN OUT UINTN  *BufferSize
-  )
-{
-  return EFI_SUCCESS;
 }
